10c0ed17f5721e5ad5acc9e4bda6b7cd50e5f692
Enrique Meneses <enrique.meneses@bayer.com>
2025-05-29 18:38:40 -0600

    domain

--- resources/auth/auth.controller.js
+++ resources/auth/auth.controller.js
@@ -20,7 +20,7 @@ const GetThreeLeggedAuth = async (req, res) => {
     const token = await GetAPSThreeLeggedToken(code);
 
     res.cookie('access_token', token, {
-      //domain: '.156041440121.cloud.bayer.com',
+      domain: '.156041440121.cloud.bayer.com',
       maxAge: 360_000_000, // ~100 hours
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
