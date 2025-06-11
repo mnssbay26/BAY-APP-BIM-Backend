d817bee434738b5d267971b8cbad697ed730a738
Enrique Meneses <enrique.meneses@bayer.com>
2025-06-10 19:15:22 -0600

    Update folders

--- utils/account_admin/all.project.users.utils.js
+++ utils/account_admin/all.project.users.utils.js
@@ -1,40 +1,27 @@
-const axios = require('axios');
+const axios = require("axios");
 
 const GetAllProjectUsers = async (token, projectId) => {
-    try {
-        if (!token) {
-            throw new Error("Unauthorized: No token provided");
-        }
+  if (!token) throw new Error("Unauthorized: No token provided");
+  if (!projectId) throw new Error("Project ID is required");
 
-        if (!projectId) {
-            throw new Error("Project ID is required");
-        }
+  let allProjectUsers = [];
+  let nextUrl = `https://developer.api.autodesk.com/construction/admin/v1/projects/${projectId}/users?limit=20&offset=0`;
 
-        let allProjectUsers = [];
-        let nextUrl = `	https://developer.api.autodesk.com/construction/admin/v1/projects/${projectId}/users?limit=20&offset=0`;
+  while (nextUrl) {
+    const { data: users } = await axios.get(nextUrl, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
 
-        while (nextUrl) {
-              const { data: users } = await axios.get(nextUrl, {
-                headers: {
-                  Authorization: `Bearer ${token}`,
-                },
-              });
-        
-              const usersWithProjectId = users.results.map((user) => ({
-                ...user,
-                projectId: projectId,
-              }));
-        
-              allProjectUsers = allProjectUsers.concat(usersWithProjectId);
-              nextUrl = users.pagination.nextUrl;
-            }
+    const usersWithProjectId = users.results.map(user => ({
+      ...user,
+      projectId,
+    }));
 
-        return allProjectUsers;
+    allProjectUsers = allProjectUsers.concat(usersWithProjectId);
+    nextUrl = users.pagination.nextUrl;
+  }
 
-    } catch (error) {
-        console.error("Error fetching project users:", error);
-        throw error;
-    }
+  return allProjectUsers;
 };
 
 module.exports = { GetAllProjectUsers };
